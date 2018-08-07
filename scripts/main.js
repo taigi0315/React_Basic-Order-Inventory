aed0e4b61017b05f9b2d9b302ca4967ea620e900
ChangikChoi <changik0315@gmail.com>
2018-08-07 18:03:35 -0500

    add gitignore

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var StorePicker = React.creaetClass({
+    render : function() {
+        <p>Hello</p>
+    }
+});
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
