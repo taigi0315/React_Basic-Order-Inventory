c6597015f3003fc7af39fbe23da17a3dd6e95990
ChangikChoi <changik0315@gmail.com>
2018-08-07 18:17:00 -0500

    add gitignore

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,13 @@ var ReactDOM = require('react-dom');
 
 var StorePicker = React.creaetClass({
     render : function() {
-        <p>Hello</p>
+        return (
+            <form>
+                <h2>Please Enter A Stroe</h2>
+                <input type='text' ref='storeId'/>
+                <input type='Submit'/>
+            </form>
+        )
     }
 });
 
